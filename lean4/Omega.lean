37641e1a690be7933e5241eacd2516944ccc4474
loning <loning.ma@aelf.io>
2026-04-09 12:16:50 +0800

    R516: GU window6 fiber non-constant rules out free action

--- lean4/Omega.lean
+++ lean4/Omega.lean
@@ -162,6 +162,7 @@ import Omega.GU.DiscriminantWedge2Square
 import Omega.GU.LeyangHolographicN2
 import Omega.GU.Window6SingleGoodPrimeRecovery
 import Omega.GU.Window6B3SupportCount
+import Omega.GU.FiberNonConstantNoFreeAction
 import Omega.CircleDimension.MultiPrimeSpectrum
 import Omega.CircleDimension.CircleDim
 import Omega.CircleDimension.ResidualCircleDim
