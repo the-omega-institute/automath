a997aa53de8fc48ebfacda57e394d3b78da93d1e
loning <loning.ma@aelf.io>
2026-04-09 10:11:44 +0800

    R504: POM A4 U4 orbit product norm identity

--- lean4/Omega.lean
+++ lean4/Omega.lean
@@ -139,6 +139,7 @@ import Omega.Core.AnomGapAmplification
 import Omega.Core.MaxFiberSplitGcdTrichotomy
 import Omega.POM.MultiplicityFixedRExtrema
 import Omega.POM.MassSplittingMomentRoot
+import Omega.POM.OrbitProductNorm
 import Omega.Core.FirstJointNonsemisimpleThreshold
 import Omega.Core.OdometerJoukowsky
 import Omega.GU.Window6RankGap
