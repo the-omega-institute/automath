527528f2c67b86d4d63b5fd9fe0627105291bae2
lexa_from_work <1621420399@qq.com>
2026-05-09 23:15:51 +0800

    pipeline supervisor: paper batch 2026-05-09T15:15:51+00:00

--- papers/publication/2026_self_dual_synchronisation_kernel_completed_determinant_cyclotomic_twists/certificates/verify_certificates.py
+++ papers/publication/2026_self_dual_synchronisation_kernel_completed_determinant_cyclotomic_twists/certificates/verify_certificates.py
@@ -185,6 +185,7 @@ def main():
         + (s**2 - 1) * w**6
     )
     record("completed_determinant", H)
+    record("resultant_m2_chosen_order_difference", s.resultant(H, s) - H(s=0))
     record("resultant_sign_m2_difference", H.resultant(s, s) + H(s=0))
 
     T = PolynomialRing(QQ, names=("x", "y"))
